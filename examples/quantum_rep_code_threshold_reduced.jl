be846451183183c053f2312e2468d23a45f3c477
Francesco Arzani <frarzani@physik.fu-berlin.de>
2024-05-09 18:53:12 +0200

    some tweaks to syndrome and candidate calculation, works suspiciously well

--- examples/quantum_rep_code_threshold_reduced.jl
+++ examples/quantum_rep_code_threshold_reduced.jl
@@ -51,26 +51,28 @@ end
     
     # println("new error")
     y= sample_error(σ, 2*n);
-
+    y[(n+1):end].=0
     M = H*J
-    syndrome = compute_syndrome(M,J,y);
+    # syndrome = compute_syndrome(M,J,y);
+    syndrome = M*J*y
+    for s in syndrome
+        s = s - round(s)
+    end
     # println(syndrome)
 
     # η = compute_eta(H, syndrome)
-    η = -G * syndrome
-    dec = zeros(size(η))
+    η = Vector(inv(M*J) * syndrome)
 
     # reduce the error candidate η to the centered parallelepiped of the dual lattice
-    η = η - Mperp_LLL'*round.(inv(Mperp_LLL')*η)
-
-    η = y
+    # η = η - Mperp_LLL'*round.(inv(Mperp_LLL')*η)
 
     # println("is this a dual lattice point? ",round.(compute_syndrome(M,J,Mperp_LLL'*round.(inv(Mperp_LLL')*η)),digits=3))
     # println(round.(compute_syndrome(M,J,η-y),digits=3))
 
-    bp_result = run_belief_propagation!(tg, η, σ/2, 50); 
+    bp_result = run_belief_propagation!(tg, η, σ/sqrt(2), 40); 
     
-    dec = hard_decision(bp_result, decision_H); 
+    # dec = hard_decision(bp_result, decision_H); 
+    dec = round.(inv(decision_H)*bp_result)
     
     # println(mod.(Mh*J*bp_result,1))
     # println(mod.(Mh*J*(bp_result-η),1))
@@ -89,28 +91,28 @@ end
     
     # dec = round.(inv(Mperp_LLL)'*η) # COMMENT THIS UNLESS YOU WANT ONLY LOCAL SEARCH
     correction = η - G*dec
-    println("the decoded vector ",round.(Int64,sqrt(2) *G*dec).%2)
-    # correction[n+1:end] .= 0 
+    # println("the decoded vector ",round.(Int64,sqrt(2) *G*dec).%2)
+    correction[n+1:end] .= 0 
     
-    # current_best_norm = norm(correction)
-    # integer_vectors = npzread("examples/local_search/integer_vectors_$n.npz")["arr_0"]
+    current_best_norm = norm(correction)
+    integer_vectors = npzread("examples/local_search/integer_vectors_$n.npz")["arr_0"]
 
-    # # for j in 1:(2*n)
-    # for j in 1:(size(integer_vectors)[1])
-    #     # for k in (-3):3
-    #         new_dec = dec[:]
-    #         # new_dec[j]= new_dec[j]+k
-    #         new_dec = new_dec + integer_vectors[j,:]
-    #         new_candidate =  η - G*new_dec
-    #         # new_candidate[n+1:end] .= 0
-    #         new_norm = norm(new_candidate)
-    #         if new_norm<current_best_norm
-    #             # println("found new best")
-    #             current_best_norm = new_norm
-    #             correction = new_candidate
-    #         end
-    #     # end
-    # end
+    # for j in 1:(2*n)
+    for j in 1:(size(integer_vectors)[1])
+        # for k in (-3):3
+            new_dec = dec[:]
+            # new_dec[j]= new_dec[j]+k
+            new_dec = new_dec + integer_vectors[j,:]
+            new_candidate =  η - G*new_dec
+            # new_candidate[n+1:end] .= 0
+            new_norm = norm(new_candidate)
+            if new_norm<current_best_norm
+                # println("found new best")
+                current_best_norm = new_norm
+                correction = new_candidate
+            end
+        # end
+    end
 
     residual = y - correction
     # println("residual: ",residual)
@@ -118,7 +120,7 @@ end
     # commutator = (residual' * J * logical)
     # success_condition = ( abs(commutator[1]-round(commutator[1])) < 1e-3)
     success_condition = !is_logical_error(code,residual)
-    # success_condition = (norm(y)>=norm(correction))
+    # success_condition = !(norm(y)<norm(correction))
 
     # residual = (y - correction)
     # success_condition = (Int64(round(sum([2*x^2 for x in residual[1:n] ])))%2==0)
@@ -150,12 +152,12 @@ matching_results = Dict(
 th_pl = plot()
 
 
-samples = 10
+samples = 10_000
 
 # for n in [5]
 
 global jj = 1
-for n in [3,5,7]
+for n in [3,7]
     println("doing n = $n")
     # println(n)
     J = symplectic_form(n)
